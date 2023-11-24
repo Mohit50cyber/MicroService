3788c3eb01fe6e2cbfbfa6190d56930f07030d5e
Mohit Singh <68845963+Mohit50cyber@users.noreply.github.com>
2023-11-24 12:50:25 +0530

    Add files via upload

---
+++ hotelservice/hotelservice/src/main/java/com/hotelservice/hotelservice/service/HotelService.java
@@ -0,0 +1,12 @@
+package com.hotelservice.hotelservice.service;
+
+import com.hotelservice.hotelservice.entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel create(Hotel hotel);
+    List<Hotel> getAll();
+    Hotel get(String id);
+}
